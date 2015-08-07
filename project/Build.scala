899d0d55e0a7fe4cfc40187d961b5d47452da27f
dhairyashil <dhairyashil.bankar@gmail.com>
2015-08-07 10:54:05 +0530

    dependency

--- project/Build.scala
+++ project/Build.scala
@@ -12,11 +12,15 @@ object ApplicationBuild extends Build {
     javaCore,
     javaJdbc,
     javaEbean,
-    "mysql" % "mysql-connector-java" % "5.1.18"
+    "mysql" % "mysql-connector-java" % "5.1.18",
+    "com.feth" %% "play-easymail" % "0.3-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here 
+   resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
+   resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns)
+  
   )
 
 }
